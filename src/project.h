516fdeb1cb7c9bc2a8a1287f0412180eec7916bf
georgegatanas <90291821+georgegatanas@users.noreply.github.com>
2021-12-11 17:13:40 -0500

    Update project.h

--- src/project.h
+++ src/project.h
@@ -1,18 +1,1 @@
-/*
- * project.h
- */
-#ifndef PROJECT_H
-#define PROJECT_H
-
-#include <stdio.h>
-
-/* DATA STRUCTURES DECLARATIONS */
-
-/* FUNCTION PROTOTYPES */
-void runSquidGame(void);
-
-void init_platform(int platform[], int levels);
-
-void init_rows(char row1[], char row2[], int levels);
-
-#endif /* PROJECT_H */
+void printGame(int levels, char *row1, char *row2);
